5f22960f33e0baf19efa7ceb081b4abfb9edf047
Yassett-Nobu <nobu027.99@gmail.com>
2020-03-10 20:52:13 -0400

    +Initial commit
    Project First Commit

---
+++ src/main/java/com/priv/ws/PasswordEncrypterSpringbootSoapApplication.java
@@ -0,0 +1,13 @@
+package com.priv.ws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PasswordEncrypterSpringbootSoapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PasswordEncrypterSpringbootSoapApplication.class, args);
+	}
+
+}
